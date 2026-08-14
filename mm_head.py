1e2f264abf3180962b15924e736f057ef6f42c9b
PumpkinJui <ZXN070320@163.com>
2026-08-14 22:26:56 +0800

    feat: 类重构、日志完善、颜色输出、容错提升、行号追踪、盔甲架适配

--- mm_head.py
+++ mm_head.py
@@ -1,106 +1,145 @@
 from base64 import urlsafe_b64decode as b64d
 from json import dump, loads
-from logging import FileHandler, Formatter, StreamHandler, getLogger
+from logging import FileHandler, Formatter, StreamHandler, getLogger, shutdown
 from pathlib import Path
 from re import search
 
 from requests import exceptions, get
 
 
-def ext(data: str) -> dict:
-    # data = 'setblock 25 74 -6 minecraft:player_head[powered=false,rotation=0]{components:{},profile:{id:[I;67411088,-739686879,-1666252800,-1432087710],name:"",properties:[{name:"textures",signature:"",value:"eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvOWYyNjg4M2RjMjhhNDljYjU4MmI2MGM2ZGNjZGNhMTczZDQ1ZjdiMWE3Yjg2OTBjYThjZTQzY2RlMGUxMzU4OSJ9fX0="}]}}'
-    loc = search(r' ([\d\- ]+) ', data).group(1)
-    try:
-        rot = search(r'rotation=(\d+)', data).group(1)
-        fac = None
-    except AttributeError:
-        rot = None
-        fac = search(r'facing=([^,\]]+)', data).group(1)
-    bsd = b64d(search(r'value:"([^\"]+)"', data).group(1)).decode()
-    url = loads(bsd)['textures']['SKIN']['url'].replace('http:', 'https:')
-    name = search(r'name:"([^"]*)"', data).group(1)
-    if not name or name in {'textures', 'Plummel'}:
-        name = url[url.rfind('/')+1:url.rfind('/')+7]
-    print(name, end='：', flush=True)
-    return {
-        'id': name,
-        'location': loc,
-        'rotation': rot,
-        'facing': fac,
-        'url': url
-    }
-
-def dls(url: str, name: str) -> bool:
-    path = Path(f'{name}.png')
-    if path.is_file():
-        print('跳过下载...', flush=True)
-        return False
-    print('开始下载...', end='', flush=True)
-    for i in range(3):
-        try:
-            img = get(url, timeout=(6.05, 10))
-            break
-        except exceptions.Timeout:
-            print(f'超时（{i+1}/3）...', end='', flush=True)
-    else:
-        print('已超时。', flush=True)
-        return False
-    with open(f'{name}.png', 'wb') as png:
-        png.write(img.content)
-    print('成功！', flush=True)
-    return True
-
-def merge(dt1: dict, dt2: dict, stem: str) -> dict:
-    dls(dt2['url'], dt2['id'])
-    dt2.pop('url')
-    if dt1.get(stem):
-        dt3 = {i['location']: i['id'] for i in dt1[stem]}
-        if not (idn := dt3.get(dt2['location'])):
-            dt1[stem].append(dt2)
+class Main:
+    def ext(self, data: str) -> dict:
+        fac, rot, url = None, None, None
+        loc = search(r' ([\d\-. ]+) ', data).group(1)
+        if 'rotation' in data:
+            rot = search(r'rotation=(\d+)', data).group(1)
+        elif 'facing' in data:
+            fac = search(r'facing=([^,\]]+)', data).group(1)
+        if 'value' in data:
+            bsr = search(r'value: ?"([^\"]+)"', data).group(1)
+            bsr += '=' * (-len(bsr) % 4)
+            bsd = b64d(bsr).decode()
+            url = loads(bsd)['textures']['SKIN']['url'].replace('http:', 'https:')
+            name = search(r'(?:name|text): ?"([^"]*)"', data).group(1)
+            if not name or name in {'textures', 'Plummel'}:
+                name = url[url.rfind('/')+1:url.rfind('/')+7]
+        elif 'head' in data:
+            name = search(r'head: ?\{[^}]*id: ?"([^"]+)"\}', data).group(1)
         else:
-            lg.warning(f'位置 {dt2["location"]} 已存在头颅 {idn}')
-        return dt1
-    dt1[stem] = [dt2]
-    return dt1
-
-def main(stem: str, data: list) -> dict:
-    dt = {}
-    for i in data:
-        if not i.strip():
-            continue
-        try:
-            dt_pending = ext(i)
-            dt = merge(dt, dt_pending, stem)
-        except Exception:
             print()
-            lg.exception('未知错误。')
-    return dt
+            lg.warning('无头颅数据。', extra={'ln': self.ln})
+            return {}
+        print(name, end='，', flush=True)
+        return {
+            'id': name,
+            'location': loc,
+            'rotation': rot,
+            'facing': fac,
+            'url': url
+        }
 
-def pre() -> None:
-    dt = {}
-    f = None
-    try:
-        for f in Path('.').glob('*.txt'):
-            stem = f.stem
-            with open(f, 'r', encoding='utf-8') as rd:
-                data = rd.read().splitlines()
-            print(stem)
-            dt |= main(stem, data)
-        if not f:
-            print('未找到 txt 后缀的批处理文件。')
-            data = [input('输入待处理项：')]
-            dt = main('info', data)
-    finally:
-        with open('info.json', 'w', encoding='utf-8') as wt:
-            dump(dt, wt)
+    def dls(self, url: str, name: str) -> bool:
+        print('开始下载...', end='', flush=True)
+        for i in range(3):
+            try:
+                img = get(url, timeout=(6.05, 10))
+                break
+            except exceptions.Timeout:
+                print(f'超时（{i+1}/3）...', end='', flush=True)
+        else:
+            print()
+            lg.warning('已超时。', extra={'ln': self.ln})
+            return False
+        with open(f'{name}.png', 'wb') as png:
+            png.write(img.content)
+        print('成功！', flush=True)
+        return True
 
+    def merge(self, dt1: dict, dt2: dict, stem: str, dl: bool=True) -> dict:
+        if not dt2:
+            return dt1
+        if dl and dt2['url'] and not Path(f'{dt2['id']}.png').is_file():
+            self.dls(dt2['url'], dt2['id'])
+        else:
+            print('跳过下载...', flush=True)
+        if dt1.get(stem):
+            dt3 = {i['location']: i['id'] for i in dt1[stem]}
+            dt4 = {i['id']: i['url'] for i in dt1[stem]}
+            if not (idn := dt3.get(dt2['location'])):
+                if dt4.get(dt2['id'], dt2['url']) != dt2['url']:
+                    lg.warning(
+                        '头颅 %s 对应多重 URL。',
+                        dt2['id'],
+                        extra={'ln': self.ln}
+                    )
+                dt1[stem].append(dt2)
+            else:
+                lg.warning(
+                    '位置 %s 已存在头颅 %s。',
+                    dt2["location"], idn,
+                    extra={'ln': self.ln}
+                )
+            return dt1
+        dt1[stem] = [dt2]
+        return dt1
+
+    def prune(self, dt: dict) -> dict:
+        dtn = {}
+        for i, j in dt.items():
+            dtn[i] = []
+            for k in j:
+                k.pop('url')
+                l = [m for m, n in k.items() if not n]
+                for m in l:
+                    k.pop(m)
+                dtn[i].append(k)
+        return dtn
+
+    def pro(self, stem: str, data: list) -> dict:
+        dt = {}
+        for i, j in enumerate(data):
+            self.ln = i + 1
+            if not j.strip():
+                continue
+            try:
+                print(f'L{self.ln}', end='：', flush=True)
+                dt_pending = self.ext(j)
+                dt = self.merge(dt, dt_pending, stem)
+            except Exception:
+                print()
+                lg.exception('未知错误。', extra={'ln': self.ln})
+        return dt
+
+    def __init__(self) -> None:
+        dt = {}
+        f = None
+        fg_reset = '\033[0m'
+        fg_bold = '\033[1m'
+        fg_blue = '\033[34m'
+        try:
+            for f in Path('.').glob('*.txt'):
+                stem = f.stem
+                with open(f, 'r', encoding='utf-8') as rd:
+                    data = rd.read().splitlines()
+                print(fg_blue, fg_bold, stem, fg_reset, ' ', len(data), sep='')
+                dt |= self.prune(self.pro(stem, data))
+            if not f:
+                print('未找到 txt 后缀的批处理文件。')
+                data = [input('输入待处理项：')]
+                dt = self.pro('info', data)
+        finally:
+            with open('info.json', 'w', encoding='utf-8') as wt:
+                dump(dt, wt)
+
+lg = getLogger(__name__)
+lg.handlers.clear()
+form = Formatter('L%(ln)s - %(levelname)s - %(message)s')
+fil_h = FileHandler('debug.log', 'w', encoding='utf-8')
+std_h = StreamHandler()
+fil_h.setFormatter(form)
+std_h.setFormatter(form)
+lg.addHandler(fil_h)
+lg.addHandler(std_h)
 if __name__ == '__main__':
-    lg = getLogger(__name__)
-    form = Formatter('%(levelname)s - %(message)s')
-    fil_h = FileHandler('debug.log', encoding='utf-8')
-    std_h = StreamHandler()
-    fil_h.setFormatter(form)
-    std_h.setFormatter(form)
-    lg.addHandler(fil_h)
-    lg.addHandler(std_h)
-    pre()
+    Main()
+shutdown()
