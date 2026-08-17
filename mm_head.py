a5495cdf7b3816344a5c216ab51b55924f14686e
PumpkinJui <ZXN070320@163.com>
2026-08-17 14:32:32 +0800

    feat: 输出格式调整、盔甲架默认不输出

--- mm_head.py
+++ mm_head.py
@@ -29,7 +29,7 @@ class Get:
             rot = int(search(r'rotation=(\d+)', data).group(1))
         elif 'facing' in data:
             fac = search(r'facing=([^,\]]+)', data).group(1)
-        if 'value' in data:
+        if 'value:' in data:
             bsr = search(r'value: ?"([^\"]+)"', data).group(1)
             bsr += '=' * (-len(bsr) % 4)
             bsd = b64d(bsr).decode()
@@ -38,7 +38,7 @@ class Get:
                 .translate(trans).lower()
             if not name or name == 'textures':
                 name = url[url.rfind('/')+1:url.rfind('/')+7]
-        elif 'head' in data:
+        elif 'head:' in data:
             name = search(r'head: ?\{[^}]*id: ?"([^"]+)"\}', data).group(1) \
                 .translate(trans).lower()
         else:
@@ -75,7 +75,7 @@ class Get:
             print()
             lg.error('已超时。', extra={'pos': f'L{self.ln} - {name}'})
             return False
-        with open(f'assets/{name}.png', 'wb') as png:
+        with open(f'output/RP/textures/entity/{name}.png', 'wb') as png:
             png.write(img.content)
         print('成功！', flush=True)
         return True
@@ -85,7 +85,7 @@ class Get:
             return dt1
         name, url = dt2['id'], dt2['url']
         if not argp().nodl and url and \
-            not Path(f'assets/{name}.png').is_file():
+            not Path(f'output/RP/textures/entity/{name}.png').is_file():
             self.dls(url, name)
         else:
             print('跳过下载...', flush=True)
@@ -113,7 +113,7 @@ class Get:
             )
             print(f'L{self.ln} - {name} - ', end='', flush=True)
             if not argp().nodl and url and \
-                not Path(f'assets/{name}.png').is_file():
+                not Path(f'output/RP/textures/entity/{name}.png').is_file():
                 self.dls(url, name)
             else:
                 print('跳过下载...', flush=True)
@@ -132,7 +132,7 @@ class Get:
                 self.urls.extend(url)
             for k in j:
                 k.pop('url')
-                l = [m for m, n in k.items() if not n]
+                l = [m for m, n in k.items() if not n and n != 0]
                 for m in l:
                     k.pop(m)
                 dtn[i].append(k)
@@ -144,6 +144,9 @@ class Get:
         for i, j in enumerate(data):
             self.ln = str(i + 1).zfill(linum)
             if not j.strip():
+                continue
+            if not argp().armorstand and 'armor_stand' in j:
+                lg.warning('盔甲架输出已关闭。', extra={'pos': f'L{self.ln}'})
                 continue
             try:
                 print(f'L{self.ln}', end=' - ', flush=True)
@@ -163,12 +166,12 @@ class Get:
         if argp().nodl:
             lg.info('跳过下载已开启。', extra={'pos': self.POS})
         else:
-            Path('assets').mkdir(parents=True, exist_ok=True)
+            Path('output/RP/textures/entity').mkdir(parents=True, exist_ok=True)
         if argp().nourl:
             lg.info('跳过 URL 记录已开启。', extra={'pos': self.POS})
         else:
             self.urls = []
-        for f in Path('./raw').glob('*.txt'):
+        for f in Path('raw').glob('*.txt'):
             stem = f.stem
             with open(f, 'r', encoding='utf-8') as rd:
                 data = rd.read().splitlines()
@@ -180,9 +183,9 @@ class Get:
         if not f:
             lg.warning('未在 raw 目录内找到 txt 后缀的批处理文件。', extra={'pos': self.POS})
             data = [input('输入待处理项：')]
-            dt = self.pro('info', data)
+            dt = self.prune(self.pro('info', data))
         with open('output/info.json', 'w', encoding='utf-8') as wt:
-            dump(dt, wt)
+            dump(dt, wt, indent=4)
         if not argp().nourl:
             past = set()
             urls = []
@@ -191,9 +194,8 @@ class Get:
                 if j not in past:
                     past.add(j)
                     urls.append(i)
-            urls = {'data': urls}
             with open('output/url.json', 'w', encoding='utf-8') as wt:
-                dump(urls, wt)
+                dump(urls, wt, indent=4)
         lg.info('信息提取完成！', extra={'pos': self.POS})
 
 class Idt:
@@ -276,7 +278,7 @@ class Idt:
             path = 'output/url.json'
             if Path(path).is_file():
                 with open(path, 'r', encoding='utf-8') as rd:
-                    data = load(rd)['data']
+                    data = load(rd)
                 lg.info('%s - L%s', path, len(data), extra={'pos': self.POS})
                 linum = len(str(len(data)))
                 for i, j in enumerate(data):
@@ -287,7 +289,7 @@ class Idt:
             lg.info('名称对照完成！', extra={'pos': self.POS})
         finally:
             with open('output/cache.json', 'w', encoding='utf-8') as wt:
-                dump(self.c_lt, wt)
+                dump(self.c_lt, wt, indent=4)
                 lg.info('缓存已输出！', extra={'pos': self.POS})
 
 class Imp:
@@ -300,7 +302,7 @@ class Imp:
             tem = rd.read()
         uni = tem.replace('yzbwdlt', idn)
         flag = templ[templ.rfind('.', 0, -7)+1:templ.rfind('.')]
-        wt_path = f'output/{flag}s/{idn}.{flag}.json'
+        wt_path = f'output/BP/{flag}s/{idn}.{flag}.json'
         self.wt_op(wt_path, uni)
         return True
 
@@ -314,36 +316,69 @@ class Imp:
         else:
             lg.warning('未找到译名文件，使用备用方案！', extra={'pos': self.POS})
         ter = '\n'.join(
-            f'"player_head_{i[0]}": {{ "textures": "textures/entity/{i[0]}" }},' \
+            f'        "player_head_{i[0]}": {{ "textures": "textures/entity/{i[0]}" }},'
             for i in idn_lt
         )
         zhl = '\n'.join(
-            f'tile.player_head:{i[0]}.name={i[1]} 的头'
+            f'tile.player_head:{i[0]}.name={
+                i[1][:i[1].rfind('_')].title()
+                if '_' in {i[1][-2], i[1][-3]}
+                else i[1].title()
+            } 的头'
             for i in idn_lt
         ) if fb else '\n'.join(
             f'tile.player_head:{i[0]}.name={
-                data.get(i[0], i[1])[:-3]
-                if data.get(i[0], i[1])[-3] == '_'
-                else data.get(i[0], i[1])
+                data.get(i[0], i[1])[:data.get(i[0], i[1]).rfind('_')].title()
+                if '_' in {data.get(i[0], i[1])[-2], data.get(i[0], i[1])[-3]}
+                else data.get(i[0], i[1]).title()
             } 的头'
             for i in idn_lt
         )
         enl = '\n'.join(
-            f"tile.player_head:{i[0]}.name={i[1]}'s Head" \
+            f"tile.player_head:{i[0]}.name={
+                i[1][:i[1].rfind('_')].title()
+                if '_' in {i[1][-2], i[1][-3]}
+                else i[1].title()
+            }'s Head"
             for i in idn_lt
         )
-        final = (
-            '// ===== terrain_texture.json =====\n' + ter +
-            '\n\n// ===== zh_CN.lang =====\n' + zhl +
-            '\n\n// ===== en_US.lang =====\n' + enl + '\n'
+        ter_full = (
+            '{\n'
+            '    "resource_pack_name": "player_head",\n'
+            '    "texture_data": {\n'
+            f'{ter}\n\n'
+            '        "player_head_yzbwdlt": { "textures": "textures/entity/yzbwdlt" },\n'
+            '        "player_head_violetmiaw": { "textures": "textures/entity/violetmiaw" },\n'
+            '        "player_head_jhy2189": { "textures": "textures/entity/jhy2189" },\n'
+            '        "player_head_freamoluwu": { "textures": "textures/entity/freamoluwu" \n}'
+            '    }'
+            '}'
         )
-        self.wt_op('output/terlang.txt', final)
+        zhl_full = (
+            '## ===== 方块 =====\n'
+            f'{zhl}\n\n'
+            'tile.player_head:yzbwdlt.name=YZBWDLT 的头\n'
+            'tile.player_head:freamoluwu.name=Freamoluwu 的头\n'
+            'tile.player_head:jhy2189.name=JHY2189 的头\n'
+            'tile.player_head:violetmiaw.name=VioletMiaw 的头\n'
+        )
+        enl_full = (
+            '## ===== Blocks =====\n'
+            f'{enl}\n\n'
+            "tile.player_head:yzbwdlt.name=YZBWDLT's Head\n"
+            "tile.player_head:freamoluwu.name=Freamoluwu's Head\n"
+            "tile.player_head:jhy2189.name=JHY2189's Head\n"
+            "tile.player_head:violetmiaw.name=VioletMiaw's Head\n"
+        )
+        self.wt_op('output/RP/textures/terrain_texture.json', ter_full)
+        self.wt_op('output/RP/texts/en_US.lang', enl_full)
+        self.wt_op('output/RP/texts/zh_CN.lang', zhl_full)
 
     def gen(self) -> None:
         block_tem = 'templates/yzbwdlt.block.json'
         item_tem = 'templates/yzbwdlt.item.json'
         block_info, item_info = True, True
-        stems = tuple(i.stem for i in Path('./assets').glob('*.png'))
+        stems = tuple(i.stem for i in Path('output/RP/textures/entity').glob('*.png'))
         if not stems:
             lg.error('无 png 文件！', extra={'pos': self.POS})
             return
@@ -399,8 +434,9 @@ class Imp:
             if old != new:
                 if re:
                     old, new = new, old
-                if Path(f'assets/{old}.png').is_file():
-                    Path(f'assets/{old}.png').replace(f'assets/{new}.png')
+                if Path(f'output/RP/textures/entity/{old}.png').is_file():
+                    Path(f'output/RP/textures/entity/{old}.png') \
+                        .replace(f'output/RP/textures/entity/{new}.png')
                 else:
                     lg.warning('%s 文件不存在，已跳过。', old, extra={'pos': ln})
                 info = info.replace(old, new)
@@ -411,7 +447,8 @@ class Imp:
                 wt.write(info)
         lg.info('重命名完成！', extra={'pos': pos})
 
-    def wt_op(self, path: str, con: str) -> None:
+    @staticmethod
+    def wt_op(path: str, con: str) -> None:
         Path(path).parent.mkdir(parents=True, exist_ok=True)
         with open(path, 'w', encoding='utf-8') as wt:
             wt.write(con)
@@ -433,6 +470,7 @@ def argp():
     par.add_argument('-l', '--nodl', action='store_true', help='跳过下载')
     par.add_argument('-u', '--nourl', action='store_true', help='跳过 URL 记录')
     par.add_argument('-c', '--nocache', action='store_true', help='忽略缓存')
+    par.add_argument('-a', '--armorstand', action='store_true', help='输出盔甲架数据')
     args = par.parse_args()
     if 'all' in args.mode:
         args.mode = ['get', 'idt', 'imp']
@@ -440,7 +478,7 @@ def argp():
         args.mode = []
     return args
 
-Path('./output').mkdir(parents=True, exist_ok=True)
+Path('output').mkdir(parents=True, exist_ok=True)
 lg = getLogger(__name__)
 lg.setLevel(DEBUG)
 lg.handlers.clear()
